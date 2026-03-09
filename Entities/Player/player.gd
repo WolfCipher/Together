88650f0f56131aa21ea5e5e79a44e53f63f9dbba
WolfCipher <rhowardsmith@wisc.edu>
2026-03-08 19:54:09 -0500

    Elvyria

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,11 +1,10 @@
-extends Node2D
+extends AnimatedSprite2D
 
+@onready var root: Node2D = $".."
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	self.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+#	pass
