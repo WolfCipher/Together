6da81a44d06c0f3cedcef877c718e8c532e81701
WolfCipher <rhowardsmith@wisc.edu>
2026-03-09 15:32:35 -0500

    Handled enemy tracking
    Follows the player closer to them

--- Entities/Enemy/enemy.gd
+++ Entities/Enemy/enemy.gd
@@ -1,19 +1,34 @@
 extends AnimatedSprite2D
 
 @onready var root: Node2D = $".."
-@export var speed = 400
+@export var speed = 200
 @export var faceDir = 0 # 0=up, 1=down, 2=right, 3=left
+@export var target1: Node2D
+@export var target2: Node2D
 
 func _ready() -> void:
 	self.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Get movement direction from WASD
-	var dir := Vector2(0,0)
-
-	# Move the sprite
-	position += dir * speed * delta
+	
+	# Determine target: follow whoever is closer
+	var pos1 = target1.global_position
+	var pos2 = target2.global_position
+	var dist1 = abs(global_position - pos1)
+	var dist2 = abs(global_position - pos2)
+	var target_pos : Vector2
+	
+	if dist1 < dist2:
+		target_pos = pos1
+	else:
+		target_pos = pos2
+	
+	# Move enemy to target
+	global_position = global_position.move_toward(target_pos, speed*delta)
+	
+	# Determine the direction and handle animations accordingly
+	var dir = (target_pos - global_position).normalized()
 
 	# Handle animations
 	if dir == Vector2.ZERO:
