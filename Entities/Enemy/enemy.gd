9217961b1439a34f0b53293ee479350c28119a8b
WolfCipher <rhowardsmith@wisc.edu>
2026-03-11 22:16:34 -0500

    Enemy Attack
    Created enemy projectile attack, attack cooldown, player damage

--- Entities/Enemy/enemy.gd
+++ Entities/Enemy/enemy.gd
@@ -10,31 +10,40 @@ extends Area2D
 @export var target1: Node2D
 @export var target2: Node2D
 @export var attackDistance := 100 # distance that the enemy needs to attack
+@export var attackFrequency := 1 # how frequent the attacks are
+@export var projectile_scene: PackedScene
+
+var attack_cooldown := 0.0
 
 func _ready() -> void:
 	play()
+	add_to_group("Enemy")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	attack_cooldown -= delta
 	
 	# Determine target: follow whoever is closer
 	var pos1 = target1.global_position
 	var pos2 = target2.global_position
-	var dist1 = abs(global_position - pos1)
-	var dist2 = abs(global_position - pos2)
-	var target_dist
+	var dist1 = global_position.distance_to(pos1)
+	var dist2 = global_position.distance_to(pos2)
+	var target_dist = min(dist1, dist2)
 	var target_pos : Vector2
 	
 	if dist1 < dist2:
-		target_dist = dist1.length()
 		target_pos = pos1
 	else:
-		target_dist = dist2.length()
 		target_pos = pos2
 	
-	# Move enemy to target, if the enemy is within a certain distance
+	# Move enemy to target, if the enemy is not within a certain distance
+	# Otherwise, attack
 	if target_dist > attackDistance:
 		global_position = global_position.move_toward(target_pos, speed*delta)
+	else:
+		if attack_cooldown <= 0:
+			attack()
+			attack_cooldown = attackFrequency
 	
 	# Determine the direction and handle animations accordingly
 	var dir = (target_pos - global_position).normalized()
@@ -74,7 +83,6 @@ func play() -> void:
 	sprite.play()
 
 # damage
-
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player Attack"):
 		health = health - 1
@@ -92,3 +100,32 @@ func damage_blink():
 		tween.tween_property(sprite, "modulate", Color(1,1,1), 0.1)
 	else:
 		tween.tween_property(sprite, "modulate", Color(0.286, 0.0, 0.0, 1.0), 0.1)
+
+func attack():
+	if self.is_in_group("ProjectileEnemy"):
+		shoot_projectile()
+	else:
+		print("Not projectile enemy")
+
+# Spawn projectiles
+func shoot_projectile() -> void:
+	var projectile = projectile_scene.instantiate()
+	var dir = get_facing_vector()
+	
+	# position slightly ahead of player and move in proper direction
+	projectile.global_position = global_position + dir * 30
+	projectile.direction = dir
+	projectile.rotation = dir.angle() + PI/2
+	
+	# spawn
+	get_tree().current_scene.add_child(projectile)
+
+# Gives the direction the player is facing
+# Ensures attacks go in the correct direction
+func get_facing_vector() -> Vector2:
+	match faceDir:
+		0: return Vector2(0,-1)
+		1: return Vector2(0,1)
+		2: return Vector2(1,0)
+		3: return Vector2(-1,0)
+	return Vector2.ZERO
--- Scenes/projectile.gd
+++ Scenes/projectile.gd
@@ -3,12 +3,16 @@ extends Area2D
 @export var speed := 1000.0
 var direction := Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if direction != Vector2.ZERO:
 		global_position += direction * speed * delta
+
+func _on_area_entered(area: Area2D) -> void:
+	var player_attack = self.is_in_group("Player Attack") && area.is_in_group("Enemy");
+	var enemy_attack = self.is_in_group("Enemy Attack") && area.is_in_group("Player");
+	
+	if player_attack || enemy_attack:
+		queue_free()
