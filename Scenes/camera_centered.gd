db0c254267458168d12f01c63bb5f1daee568900
WolfCipher <rhowardsmith@wisc.edu>
2026-03-22 18:50:14 -0500

    Camera, Player Attack Cooldown
    camera moves at center of the 2 players; players cannot go outside the camera
    certain player attacks cannot be done as frequently

---
+++ Scenes/camera_centered.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+@export var speed = 400
+@export var target1: Node2D
+@export var target2: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Move to average of targets
+func _process(delta: float) -> void:
+	var pos1 = target1.global_position
+	var pos2 = target2.global_position
+	var x = (pos1.x + pos2.x)/2
+	var y = (pos1.y + pos2.y)/2
+	var target_pos = Vector2(x,y)
+	
+	global_position = global_position.move_toward(target_pos, speed*delta)
