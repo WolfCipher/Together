5458fb54d20fb4eafae66b8aedf892b39d53e4bf
WolfCipher <rhowardsmith@wisc.edu>
2026-04-02 19:06:27 -0500

    SceneCache
    faster scene load in-game; longer game intro load

---
+++ Scenes/Functionality/SceneCache.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var level1 : PackedScene
+var dialog1 : PackedScene
+var level2 : PackedScene
---
+++ Scenes/UI Scenes/menu_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SceneCache.level1 = preload("res://Scenes/Levels/level1.tscn")
+	SceneCache.dialog1 = preload("res://Ink/Scenes/ink_scene.tscn")
+	SceneCache.level2 = preload("res://Scenes/Levels/level2.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
