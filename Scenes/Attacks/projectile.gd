bd7aa36bbac93d9e89792474f9b49683762d6452
WolfCipher <rhowardsmith@wisc.edu>
2026-04-17 14:40:22 -0500

    Melee/Projectile Combo
    can upgrade other players' attacks by letting their projectile go through your melee

--- Scenes/Attacks/projectile.gd
+++ Scenes/Attacks/projectile.gd
@@ -21,8 +21,22 @@ func play() -> void:
 	audio.play()
 
 func _on_area_entered(area: Area2D) -> void:
+	
 	var player_attack = self.is_in_group("Player Attack") && area.is_in_group("Enemy");
 	var enemy_attack = self.is_in_group("Enemy Attack") && area.is_in_group("Player");
 	
 	if player_attack || enemy_attack:
 		queue_free()
+	
+	var ryl_enhance = self.is_in_group("RylProjectile") && area.is_in_group("ElvyriaMelee");
+	var elvyria_enhance = self.is_in_group("ElvyriaProjectile") && area.is_in_group("RylMelee");
+	
+	if (ryl_enhance || elvyria_enhance):
+		damage += 2;
+		
+		var tween = create_tween()
+		if (ryl_enhance):
+			tween.tween_property(sprite, "modulate", Color(2,2,0.5), 0.1)
+		else:
+			tween.tween_property(sprite, "modulate", Color(1,1.5,100), 0.1)
+		
