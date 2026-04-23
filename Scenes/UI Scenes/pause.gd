8e9d4985d8bdb0251c3dcdf51ab2e34480e6bdf0
TuxieNight <phowardsmith@wisc.edu>
2026-04-23 17:26:39 -0500

    Added heart health to level 4

--- Scenes/UI Scenes/pause.gd
+++ Scenes/UI Scenes/pause.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var startsVisible = false # only level 1 is true
 var volume_change = .5
+@onready var music = "$../../CoreLoop"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +11,6 @@ func _ready() -> void:
 		get_tree().paused = true
 		if music:
 			music.volume_db -= volume_change
-		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"),  AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Music")) - volume_change)
 		await get_tree().create_timer(2.0).timeout
 
 
