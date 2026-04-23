2ba936ff17420e24c8dd33d0b6b85a56aad50af1
cobright-wisc <cobright@wisc.edu>
2026-04-23 13:21:41 -0500

    Added Button SFX

--- Scenes/UI Scenes/scene_button.gd
+++ Scenes/UI Scenes/scene_button.gd
@@ -5,14 +5,14 @@ extends Button
 @onready var btn_click = AudioStreamPlayer.new()
 @onready var btn_hover = AudioStreamPlayer.new()
 
-var click_sfx = preload("res://Sound/SFX/UI_Sounds/Button_Select/Button_Select_1.wav")
-var hover_sfx = preload("res://Sound/SFX/UI_Sounds/Button_Hover/Button_Hover_1.wav")
+var click_sfx = preload("res://Sound/SFX/UI_Sounds/Button_Select/UI_Click_1.wav")
+var hover_sfx = preload("res://Sound/SFX/UI_Sounds/Button_Hover/UI_Hover_1.wav")
 
 var hover_playing = false
+var focused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 	btn_click.stream = click_sfx
 	btn_hover.stream = hover_sfx
 	add_child(btn_click)
@@ -33,4 +33,8 @@ func _process(_delta: float) -> void:
 		btn_hover.play()
 	if !is_hovered():
 		hover_playing = false
-	
+	if has_focus() && !focused:
+		focused = true
+		btn_hover.play()
+	if !has_focus():
+		focused = false
